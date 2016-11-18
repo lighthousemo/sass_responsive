cc9088e83d8980fd934f0e859ded6d57a1b46098
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-18 08:56:07 -0800

    Serve assets from public folder

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 8000; // default port 8000
 let contacts = require("./contact-data")
 
+// use ejs views from the views folder
 app.set("view engine", "ejs");
+
+// serve static assets (css, images, etc) from public folder
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   // render the views/index.ejs file
