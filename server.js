3a82457c814c47455f6f0ea9b10fcc9949e789a9
Monica Olinescu <monica.olinescu@gmail.com>
2017-04-20 20:40:10 -0700

    Improve starter code

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8000; // default port 8000
-let contacts = require("./contact-data")
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000 // default port 8000
+let contacts = require('./contact-data')
 
 // use ejs views from the views folder
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 
 // serve static assets (css, images, etc) from public folder
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // render the views/index.ejs file
-  res.render("index", { contacts })
-});
+  res.render('index', {contacts: contacts, contact: undefined})
+})
+
+app.get('/contact/:contact_id', (req, res) => {
+  // render views/index.ejs file with a selected contact
+  const currentContact = contacts.find(c => c.id === parseInt(req.params.contact_id))
+  console.log(currentContact)
+  res.render('index', {
+    contacts: contacts,
+    contact: currentContact
+  })
+})
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+  console.log(`Example app listening on port ${PORT}!`)
+})
